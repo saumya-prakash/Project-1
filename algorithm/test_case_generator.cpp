9225cb69dc6f0895efb16be5290d2a423667b8eb
saumya-prakash <saumyaphonu@gmail.com>
2021-11-19 11:28:40 +0530

    a random test case generator written

---
+++ algorithm/test_case_generator.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+
+    int m = 50;
+    int n = 200;
+
+    vector<int> construction_cost(m, 0);
+    for(int i=0; i<m; i++)
+        construction_cost[i] = rand()%100 + 1;
+
+
+    vector<vector<int>> distance_cost(n, vector<int>(m, 0));
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+            distance_cost[i][j] = rand()%60 + 1;
+
+    // write to a file
+    ofstream fi;
+    fi.open("test3", ofstream::out);
+
+    fi<<m<<" "<<n<<endl;
+    for(int i=0; i<m; i++)
+        fi<<construction_cost[i]<<" ";
+    
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+            fi<<distance_cost[i][j]<<" ";
+        fi<<endl;
+    }
+
+    fi.close();
+    return 0;
+}
