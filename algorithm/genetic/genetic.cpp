4ccfcc64749c5e34f9b3a5b0456c5ef6bd7c3f86
saumya-prakash <saumyaphonu@gmail.com>
2022-04-01 17:16:57 +0530

    refactoring

--- algorithm/genetic/genetic.cpp
+++ algorithm/genetic/genetic.cpp
@@ -1,5 +1,116 @@
 #include "genetic.h"
 
+
+void GeneticAlgorithm::solve()
+{
+    // // Create the initial chromosomes
+    // population = vector<vector<int>>();
+    // value = vector<int>();
+
+
+    int ini_num = 10;
+
+    for(int i=0; i<ini_num; i++)
+    {
+        vector<int> tmp = random_chromosome();
+        population.push_back(tmp);
+
+        int a = objective_function(tmp);
+        total += a;
+        value.push_back(a);
+    }
+
+
+    // Run for 'iter' iterations
+    
+    const int N = 10000;
+    int iter = 1;
+    // O(N*[N + mlog(m) + n])
+    while(iter <= N)
+    {
+        // cout<<"Iteration: "<<iter<<"..."<<endl;
+
+        // // Select 2 chromosomes
+        pair<vector<int>, vector<int>> individuals = select();
+
+        // 2-point crossover
+        pair<vector<int>, vector<int>> children = crossover(individuals.first, individuals.second);
+
+        // Validate, and mutate if something wrong
+        validate(children.first);
+        validate(children.second);
+
+
+        // Add to the population
+        population.push_back(children.first);
+        int a = objective_function(children.first);
+        total += a;
+        value.push_back(a);
+
+        population.push_back(children.second);
+        a = objective_function(children.second);
+        total += a;
+        value.push_back(a);
+
+        iter++;  
+    }
+
+
+    return;
+}
+
+
+
+// This function calculates the objective function on the given chromosome 
+// m*log(m) + m*log(m) + n
+// O(m*log(m) + n) 
+int GeneticAlgorithm::objective_function(vector<int> &chromosome) const
+{
+    int res = 0;  // to store the result
+
+    vector<int> aux;
+    for(int i=0; i<m; i++)
+        aux.push_back(chromosome[i]);
+    sort(aux.begin(), aux.end());
+
+
+    for(int i=0; i<m; i++)
+    {
+        if(chromosome[i] >= 0)
+        {
+            // cost of constructing the ith station
+            res += construction[i];
+
+            // Find the next largest number in the first half of the chromosome
+            
+            int key = chromosome[i];
+            // int a = INT_MAX;
+
+            // // LINEAR approach
+            // for(int j=0; j<m; j++)
+            // {
+            //     if(chromosome[j] > key && chromosome[j] < a)
+            //         a = chromosome[j];
+            // }
+
+            // if(a == INT_MAX)
+            //     a = n;
+
+            // LOGARITHMIC approach            
+            int a = locate(key, aux);
+
+            for(int j=key; j<a; j++)
+                // assign client-j to the station-i
+                res += weight[j][i];
+        }
+    }
+
+    return res;
+}
+
+
+
+
 // This function creates a random chromosome and returns it
 // O(m+n)
 vector<int> GeneticAlgorithm::random_chromosome()
@@ -47,132 +158,51 @@ vector<int> GeneticAlgorithm::random_chromosome()
 
 
 
-// This function permutates the given array range
-// linear time complexity
-void GeneticAlgorithm::permutate(int begin, int end, vector<int> &arr)
+pair<vector<int>, vector<int>> GeneticAlgorithm::select()
 {
-    // permutate the elements in the range [begin, end)
-
-    for(int i=end-1; i>=begin; i--)
-    {
-        int n = i-begin+1;
-
-        int ind = rand()%n + begin;
-
-        swap(arr[ind], arr[i]);
-    }
-
-    return;
-}
-
-
-
-// This function prints the chromosome
-// O(m+n)
-void GeneticAlgorithm::print_chromosome(const vector<int> &chromosome) const
-{
-    for(int i=0; i<m; i++)
-        cout<<chromosome[i]<<" ";
-
-    cout<<"| ";
+    // Select 2 chromosomes
     
-    for(int i=m; i<chromosome.size(); i++)
-        cout<<chromosome[i]<<" ";
+    int ind1 = -1, ind2 = -1;
+
+    // use roulette wheel selection
+
+    // First individual
+    int target = drand48()*total;
+    int sum = 0;
     
-    cout<<endl;
-
-    return;
-}
-
-
-
-// O(log m)
-int GeneticAlgorithm::locate(int key, const vector<int> &arr) const
-{
-    int low = 0;
-    int high = arr.size()-1;
-
-    int a = INT_MAX;
-
-    while(low <= high)
+    for(int i=0; i<population.size(); i++)
     {
-        int mid = low + (high-low)/2;
-
-        if(arr[mid] == key)
+        sum += value[i];
+        if(sum >= target)
         {
-            a = mid;
+            ind1 = i;
             break;
         }
-
-        else if(arr[mid] > key)
-            high = mid-1;
-        
-        else
-            low = mid+1;
     }
 
-    if(a == arr.size()-1)
-        return n;
+
+    // Second individual
+    target = drand48()*(total-value[ind1]);
+    sum = 0;
     
-    return arr[a+1];
-}
-
-
-
-// This function calculates the objective function on the given chromosome 
-// m*log(m) + m*log(m) + n
-// O(m*log(m) + n) 
-int GeneticAlgorithm::objective_function(vector<int> &chromosome) const
-{
-    int total = 0;  // to store the value
-
-    vector<int> aux;
-    for(int i=0; i<m; i++)
-        aux.push_back(chromosome[i]);
-    sort(aux.begin(), aux.end());
-
-    // for(int i=0; i<m; i++)
-    //     cout<<chromosome[i]<<" ";
-    // cout<<endl;
-
-    for(int i=0; i<m; i++)
+    for(int i=0; i<population.size(); i++)
     {
-        if(chromosome[i] >= 0)
+        if(i != ind1)
+            sum += value[i];
+
+        if(sum >= target)
         {
-            // const of constructing the ith station
-            total += construction[i];
-
-            // Find the next largest number in the first half of the chromosome
-            
-            int key = chromosome[i];
-            // int a = INT_MAX;
-            // for(int j=0; j<m; j++)
-            // {
-            //     if(chromosome[j] > key && chromosome[j] < a)
-            //         a = chromosome[j];
-            // }
-
-            // if(a == INT_MAX)
-            //     a = n;
-            
-            int a = locate(key, aux);
-            // cout<<key<<"->"<<a<<" "<<b<<endl;
-            // assert(a == b);
-
-            for(int j=key; j<a; j++)
-            {
-                // Assign client-j to the station-i
-                total += weight[j][i];
-            }
+            ind2 = i;
+            break;
         }
     }
-
-    return total;
+    
+    return {population[ind1], population[ind2]};
 }
 
 
-// This functnio does crossover between 2 chromosomes and returns 2 children
-// O(m+n)
+// Does crossover between 2 chromosomes and returns 2 children
+// O(m+n) time
 pair<vector<int>, vector<int>> GeneticAlgorithm::crossover(vector<int> &chr1, vector<int> &chr2)
 {
     vector<int> child_1(m+n, -1);
@@ -208,16 +238,39 @@ pair<vector<int>, vector<int>> GeneticAlgorithm::crossover(vector<int> &chr1, ve
         child_2[i] = chr1[i];
     }
 
-    pair<vector<int>, vector<int>> res;
-    res.first = child_1;
-    res.second = child_2;
+    pair<vector<int>, vector<int>> children;
+    children.first = child_1;
+    children.second = child_2;
 
-    return res;
+    return children;
 }
 
 
-// This function validates a given chromosome
-// O(m+n)
+
+// This function permutates the given array range
+// linear time complexity
+void GeneticAlgorithm::permutate(int begin, int end, vector<int> &arr)
+{
+    // permutate the elements in the range [begin, end)
+
+    for(int i=end-1; i>=begin; i--)
+    {
+        int n = i-begin+1;
+
+        int ind = rand()%n + begin;
+
+        swap(arr[ind], arr[i]);
+    }
+
+    return;
+}
+
+
+
+
+
+// Does validation of the chromosome
+// O(m+n) time
 void GeneticAlgorithm::validate(vector<int> &chromosome)
 {
     // Check the station part
@@ -234,6 +287,7 @@ void GeneticAlgorithm::validate(vector<int> &chromosome)
             aux[cur]++;
     }
 
+
     // remove duplicates
     for(int i=0; i<m; i++)
     {
@@ -241,10 +295,11 @@ void GeneticAlgorithm::validate(vector<int> &chromosome)
 
         if(cur >= 0 && aux[cur] > 1)    // cur occurs more than once
         {
-            chromosome[i] = -1;         // make this entry of cur -1
+            chromosome[i] = -1;         // make this entry of cur -1 [Any other strategy may be thought about]
             aux[cur]--;                 // decrement the total count of cur
         }
     }
+
 
     // if 0 is not present, insert it at some random location
     if(aux[0] == 0)
@@ -305,114 +360,54 @@ void GeneticAlgorithm::validate(vector<int> &chromosome)
 }
 
 
-
-/*************************************************************************************
- * MAIN function ahead
- * ***********************************************************************************/
-
-void GeneticAlgorithm::solve()
+// O(log m)
+int GeneticAlgorithm::locate(int key, const vector<int> &arr) const
 {
-    // // Create the initial chromosomes
-    // population = vector<vector<int>>();
-    // value = vector<int>();
+    int low = 0;
+    int high = arr.size()-1;
 
+    int a = INT_MAX;
 
-    int total = 0;  // to store the total fitness value of the population
-    int ini_num = 10;
-    for(int i=0; i<ini_num; i++)
+    while(low <= high)
     {
-        vector<int> tmp = random_chromosome();
-        population.push_back(tmp);
+        int mid = low + (high-low)/2;
 
-        int a = objective_function(tmp);
-        total += a;
-        value.push_back(a);
-    }
+        if(arr[mid] == key)
+        {
+            a = mid;
+            break;
+        }
 
-
-    // Run for 'iter' iterations
-    
-    const int N = 10000;
-    int iter = 1;
-    // O(N*[N + mlog(m) + n])
-    while(iter <= N)
-    {
-        // cout<<"Iteration: "<<iter<<"..."<<endl;
-
-        // Select 2 chromosomes
-        vector<int> chr1, chr2;
-
+        else if(arr[mid] > key)
+            high = mid-1;
         
-        // for(int i=0; i<population.size() && cnt>0; i++)
-        // {
-        //     int cur = value[i];
-
-        //     double prob = 1 - (double)value[i] / total;
-
-        //     if(drand48() <= prob)
-        //     {
-        //         chr2 = chr1;
-        //         chr1 = population[i];
-
-        //         cnt--;
-        //     }
-        // }
-
-
-        // maybe use roulette wheel selection
-        int cnt = 2;
-        int target = drand48()*total;
-        int sum = 0;
-        for(int i=0; i<population.size(); i++)
-        {
-            sum += value[i];
-            if(sum >= target)
-            {
-                chr1 = population[i];
-                break;
-            }
-        }
-
-        target = drand48()*total;
-        sum = 0;
-        for(int i=0; i<population.size(); i++)
-        {
-            sum += value[i];
-            if(sum >= target)
-            {
-                chr2 = population[i];
-                break;
-            }
-        }
-
-        if(chr1.empty() || chr2.empty())
-        {
-            iter++;
-            continue;
-        }
-
-        // 2-point Crossover
-        pair<vector<int>, vector<int>> children = crossover(chr1, chr2);
-
-        // Validate, and mutate if something wrong
-        validate(children.first);
-        validate(children.second);
-
-
-        // Add to the population
-        population.push_back(children.first);
-        int a = objective_function(children.first);
-        total += a;
-        value.push_back(a);
-
-        population.push_back(children.second);
-        a = objective_function(children.second);
-        total += a;
-        value.push_back(a);
-
-        iter++;  
+        else
+            low = mid+1;
     }
 
+    if(a == arr.size()-1)
+        return n;
+    
+    return arr[a+1];
+}
+
+
+
+
+
+// This function prints the chromosome
+// O(m+n)
+void GeneticAlgorithm::print_chromosome(const vector<int> &chromosome) const
+{
+    for(int i=0; i<m; i++)
+        cout<<chromosome[i]<<" ";
+
+    cout<<"| ";
+    
+    for(int i=m; i<chromosome.size(); i++)
+        cout<<chromosome[i]<<" ";
+    
+    cout<<endl;
 
     return;
 }
--- algorithm/genetic/genetic.h
+++ algorithm/genetic/genetic.h
@@ -9,16 +9,18 @@ private:
     int m, n;   // number of candidate sites, number of clients
     vector<int> construction;
     vector<vector<int>> weight;
+    int total;  // to store the sum of values in value[]
 
 
 public:
     GeneticAlgorithm(int _m, int _n, vector<int> &_construction, vector<vector<int>> &_weight):
-        m(_m), n(_n), construction(_construction), weight(_weight)
+        m(_m), n(_n), construction(_construction), weight(_weight), total(0)
     {}
 
 
     vector<vector<int>> population;
     vector<int> value;
+
 
     void solve();
 
@@ -29,6 +31,7 @@ private:
 
     vector<int> random_chromosome();
 
+    pair<vector<int>, vector<int>> select();
     void permutate(int begin, int end, vector<int> &arr);
 
     pair<vector<int>, vector<int>> crossover(vector<int> &chr1, vector<int> &chr2);
